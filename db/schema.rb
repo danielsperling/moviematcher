45b4be73a681e82d1434de002b8245af657d8766
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-09 15:10:27 +0100

    add movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_234439) do
+ActiveRecord::Schema.define(version: 2021_02_09_140912) do
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.integer "rating"
+    t.string "poster"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
