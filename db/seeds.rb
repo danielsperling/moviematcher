da8bdaa13ca5116f03d19510f8b2165900d4d8db
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-11 01:30:07 +0100

    new movie seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,15 @@ print 'created'
 sexy_beast = Movie.create(title: 'Sexy Beast', rating: 7.3, poster: File.open(Rails.root + 'app/assets/images/Sexybeast.jpg'), description: 'Brutal gangster Don Logan recruits "retired" safecracker Gal for one last job, but it goes badly for both of them.')
 sexy_beast.save!
 puts 'Movie Created'
+
+star_wars = Movie.create(title: 'Star Wars: Episode IV', rating: 8.6, poster: File.open(Rails.root + 'app/assets/images/Sexybeast.jpg'), description: 'Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a Wookiee and two droids to save the galaxy from the Empires world-destroying battle station, while also attempting to rescue Princess Leia from the mysterious Darth Vader')
+star_wars.save!
+puts 'Movie Created'
+
+pulp_fiction = Movie.create(title: 'Pulp Fiction (', rating: 8.9, poster: File.open(Rails.root + 'app/assets/images/Sexybeast.jpg'), description: 'The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption..')
+pulp_fiction.save!
+puts 'Movie Created'
+
+the_matrix = Movie.create(title: 'The Matrix', rating: 7.3, poster: File.open(Rails.root + 'app/assets/images/Sexybeast.jpg'), description: 'When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence.')
+the_matrix.save!
+puts 'Movie Created'
