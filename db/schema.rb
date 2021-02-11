759434cc46fa04ccab7c3b9e0cbb28584b52f854
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-11 01:16:58 +0100

    add bio to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_000617) do
+ActiveRecord::Schema.define(version: 2021_02_11_001634) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_000617) do
     t.string "name"
     t.string "username"
     t.string "avatar"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
