3fa413ed643014c9464cb5d1e15e5a090e2bcddd
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-18 23:09:53 +0100

    trying to make it woooork

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,13 @@
-require 'httparty'
 require 'json'
+require 'httparty'
 
 BASE_URL = 'http://www.omdbapi.com/?t='.freeze
 API_KEY = '&apikey=8404523a'.freeze
 
 class MoviesController < ApplicationController
   def index
-    @response = HTTParty.get(BASE_URL + 'rambo' + API_KEY).parsed_response
+    response = HTTParty.get(BASE_URL + 'rambo' + API_KEY)
+    @data = JSON.parse(response.body)
   end
 end
 
