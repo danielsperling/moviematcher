335418c4e4a21477773419dc81d88b2bfcee4d45
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-18 12:05:29 +0100

    add api call

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,18 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class MoviesController < ApplicationController
+  url = URI('https://movie-database-imdb-alternative.p.rapidapi.com/?s=Avengers%20Endgame&page=1&r=json')
+
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  request = Net::HTTP::Get.new(url)
+  request['x-rapidapi-key'] = '9808f43b20msh42d416b8692f637p127437jsn1804a8c6b03b'
+  request['x-rapidapi-host'] = 'movie-database-imdb-alternative.p.rapidapi.com'
+
+  response = http.request(request)
+  puts response.read_body
 end
