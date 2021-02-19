f178dc10aea5e213f265f14cb908452947607373
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-19 20:49:51 +0100

    get more than 10 results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,14 @@
 require 'json'
 require 'httparty'
 
-BASE_URL = 'http://www.omdbapi.com/?s='.freeze
+BASE_URL = 'http://www.omdbapi.com/?t='.freeze
 API_KEY = '&apikey=8404523a'.freeze
+PARAM = '&plot=full&'.freeze
 
 class MoviesController < ApplicationController
   def index
-    response = HTTParty.get(BASE_URL + 'harry' + API_KEY)
-    @data = JSON.parse(response.body)
+    response = HTTParty.get(BASE_URL + 'star' + PARAM + API_KEY)
+    @raw_data = JSON.parse(response.body)
+    @data = @raw_data['Search']
   end
 end
