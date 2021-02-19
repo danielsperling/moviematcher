23e21988b487e462ccc6f1378ddb07bb0cbb9af0
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-19 02:50:55 +0100

    can't find it

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,14 +1,12 @@
 require 'json'
 require 'httparty'
 
-BASE_URL = 'http://www.omdbapi.com/?t='.freeze
+BASE_URL = 'http://www.omdbapi.com/?s='.freeze
 API_KEY = '&apikey=8404523a'.freeze
 
 class MoviesController < ApplicationController
   def index
-    response = HTTParty.get(BASE_URL + 'rambo' + API_KEY)
+    response = HTTParty.get(BASE_URL + 'harry' + API_KEY)
     @data = JSON.parse(response.body)
   end
 end
-
-# http://www.omdbapi.com/?i=tt3896198&apikey=8404523a
