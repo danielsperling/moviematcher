a62a021fe2e6c5664012ff42865edba6b7fe3cc6
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-19 22:29:28 +0100

    add random effect

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,29 +12,31 @@ PAGE5 = '"&page=5'.freeze
 PAGE6 = '"&page=6'.freeze
 PAGE7 = '"&page=7'.freeze
 
+RANDOM_MOVIE = %w[harry star piratesRANDOM_MOVIErambo].sample
+
 class MoviesController < ApplicationController
   def index
-    response_1 = HTTParty.get(BASE_URL + 'star' + PAGE1 + API_KEY)
+    response_1 = HTTParty.get(BASE_URL + RANDOM_MOVIE + PAGE1 + API_KEY)
     @raw_data_1 = JSON.parse(response_1.body)
     @data_1 = @raw_data_1['Search']
 
-    response_2 = HTTParty.get(BASE_URL + 'star' + PAGE2 + API_KEY)
+    response_2 = HTTParty.get(BASE_URL + RANDOM_MOVIE + PAGE2 + API_KEY)
     @raw_data_2 = JSON.parse(response_2.body)
     @data_2 = @raw_data_2['Search']
 
-    response_3 = HTTParty.get(BASE_URL + 'star' + PAGE3 + API_KEY)
+    response_3 = HTTParty.get(BASE_URL + RANDOM_MOVIE + PAGE3 + API_KEY)
     @raw_data_3 = JSON.parse(response_3.body)
     @data_3 = @raw_data_3['Search']
 
-    response_4 = HTTParty.get(BASE_URL + 'star' + PAGE4 + API_KEY)
+    response_4 = HTTParty.get(BASE_URL + RANDOM_MOVIE + PAGE4 + API_KEY)
     @raw_data_4 = JSON.parse(response_4.body)
     @data_4 = @raw_data_4['Search']
 
-    response_5 = HTTParty.get(BASE_URL + 'star' + PAGE5 + API_KEY)
+    response_5 = HTTParty.get(BASE_URL + RANDOM_MOVIE + PAGE5 + API_KEY)
     @raw_data_5 = JSON.parse(response_5.body)
     @data_5 = @raw_data_5['Search']
 
-    response_6 = HTTParty.get(BASE_URL + 'star' + PAGE6 + API_KEY)
+    response_6 = HTTParty.get(BASE_URL + RANDOM_MOVIE + PAGE6 + API_KEY)
     @raw_data_6 = JSON.parse(response_6.body)
     @data_6 = @raw_data_6['Search']
 
