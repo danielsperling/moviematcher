beee0ea7db7c544e989c8f66452b4a5ed2d51719
Daniel Sperling <daniel_sperling@yahoo.com>
2021-02-23 19:12:55 +0100

    fix display of movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,3 @@ Movie.create_movies(movies)
 
 
 
-<div class="row"> 
-<#% @movies.each do |display|  %>
-        <div class="col-12" id="movie-card">
-                <%= image_tag display.poster, class: 'movie-poster' %>
-
-        <div class="overlay overlay--blur">
-            <h3><%= display.title %></h3>
-            <h4>⭐️ <%= display.year %></h4>
-            <div>
-            <h5><%= display.plot %></h5>
-            </div>
-        </div>
-        </div>
-
-    <#% end %>
-
-<#% end %>
-</div>
