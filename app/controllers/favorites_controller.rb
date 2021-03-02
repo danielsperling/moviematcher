475e756b7b2b25bcc88e2d879d0d0cf560b52e9f
Daniel Sperling <daniel_sperling@yahoo.com>
2021-03-02 14:09:51 +0100

    trying to fix favorite function

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,17 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all
+  end
+
+  def create
+    @favorite = Favotire.new
+    @movie = Movie.find(params[:id])
+    @favorite.user = current_user
+    @favorite.movie = @movie
+    if @favorite.save
+      redirect_to movies_path
+    else
+      render_new
+    end
+  end
 end
