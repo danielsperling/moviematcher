5ffd9f59e50fa3a33352eb90ab87abd08b06278d
Daniel Sperling <daniel_sperling@yahoo.com>
2021-03-04 14:08:34 +0100

    fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :favorites, dependent: :destroy
-  has_many :users, through: :favorites
+  has_many :favorite_movies
+  has_many :favorited_by, through: :favorite_movies, source: :user
 
   def self.create_movies(movies)
     movies.each do |movie_hash|
