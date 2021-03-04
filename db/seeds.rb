831354dfc6c3b1c797fbb6b0729574edcc206e0b
Daniel Sperling <daniel_sperling@yahoo.com>
2021-03-04 17:39:24 +0100

    remove genre from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,3 @@ Genre.destroy_all
 scrape = Scraper.new
 movies = scrape.scraper
 Movie.create_movies(movies)
-
-puts 'Creating Genres'
-action = Genre.create(genre: 'Action')
-action.save!
-
-romance = Genre.create(genre: 'Romance')
-romance.save!
-
-comedy = Genre.create(genre: 'Comedy')
-comedy.save!
-
-thriller = Genre.create(genre: 'Thriller')
-thriller.save!
-
-fantasy = Genre.create(genre: 'Fantasy')
-fantasy.save!
-puts 'Genres Created'
